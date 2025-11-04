87479f420eb8a960ca98e613270878df80183776
jayedbinnazir <jayed.freelance@gmail.com>
2025-11-04 20:55:08 +0600

    adding global platform agnistic exception Filter

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpException } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,6 +7,7 @@ export class AppController {
 
   @Get()
   getHello(): any {
+    throw new HttpException('Just Testing Exception Filters', 400);
     return {
       msg: `requested by ${process.env.APP_NAME}`,
     };
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
+import { CustomExceptionFilter } from './common/filters/global-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const httpAdapterHost = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new CustomExceptionFilter(httpAdapterHost));
+
   const configservice = app.get(ConfigService);
   const PORT = configservice.get<number>('app.port') as number;
   const HOST = configservice.get<string>('app.host') as string;
